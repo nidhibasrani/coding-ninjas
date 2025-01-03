391880546b5bf24ccbbc2c46d856991add123a67
Nidhi <nidhibasrani54@gmail.com>
2025-01-03 18:21:37 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
-import { Outlet } from "react-router-dom"
+
 export default function App() {
   return (
     <div>
