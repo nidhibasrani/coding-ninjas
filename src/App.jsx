4c70f2101684de947a2b58d2d4b82ac726d18f9b
Gourangh <gourangh2@gmail.com>
2025-01-03 18:17:43 +0530

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
-import { Outlet } from "react-router-dom"
+
 export default function App() {
   return (
     <div>
       <Header/>
-      <Outlet/>
+      <h1>Hello Testing tailwind</h1>
+ 
       <Footer/>
 
     </div>
