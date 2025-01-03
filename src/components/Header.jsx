5207929c264d247e8cb956457045b7215666561f
Nidhi <nidhibasrani54@gmail.com>
2025-01-03 18:57:51 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="parent flex justify-around py-2 items-center">
+      <div className="left flex justify-around gap-6 items-center">
+        <div className="logo">
+          <img src="./public/logo.svg " height={150} width={150} />
+        </div>
+        <div>
+          <ul className="flex gap-6">
+            <li className="font-mulish font-bold ">Job bootcamp</li>
+            <li className="font-mulish font-bold ">IIT Certifications</li>
+          </ul>
+        </div>
+      </div>
+      <div className="right">
+        <button className="bg-orange text-white text-sm rounded-lg p-2 font-mulish">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
